ec4fa4a88ab9d88d01132f405e41c7425c955847
nahirmaraz <marlen.maraz@gmail.com>
2024-01-08 18:49:39 -0300

    Main

---
+++ API/main.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from API.funciones import UserForGenre,SentimentAnalysis,RecomendacionJuego
+from fastapi import FastAPI
+
+#Inicializamos
+app = FastAPI()
+app.title='Steam Games'
+@app.get("/", tags=['Inicio'])
+def bienvenida():
+   return 'Bienvenidos'
+
+@app.get("/user_for_genre/{genero}", tags=['Consultas'])
+async def user_for_genre (genero:str):
+    diccionario=UserForGenre(genero)
+    return diccionario
+
+@app.get("/sentiment_analysis/{developer}", tags=['Consultas'])
+async def sentiment_analysis (developer:str):
+    dicc=SentimentAnalysis (developer)
+    return dicc
+
+@app.get("/recommended_games/{id}", tags=['Sistema de recomendación'])
+async def recomendacion_juego(id: int, top_n: int = 5):
+    # Llamar directamente a la función RecomendacionJuego con los parámetros proporcionados
+    recommended_games = RecomendacionJuego(id, top_n)
+        
+    # Devolver los resultados como respuesta de la API
+    return {"JuegosRecomendados": recommended_games.to_list()}
